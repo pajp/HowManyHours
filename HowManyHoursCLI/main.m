24e336cae39903362f20a7d45bb1d600ca029653
Rasmus Sten <rasmus@dll.nu>
2017-07-18 15:20:02 +0300

    Initial Commit

---
+++ HowManyHoursCLI/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HowManyHoursCLI
+//
+//  Created by Rasmus Sten on 18-07-2017.
+//  Copyright © 2017 Rasmus Sten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
